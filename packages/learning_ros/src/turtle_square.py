e6eaceea3dcda982b66efe6f429802b54dfb50f1
“Daniel <danielkaijzer00@gmail.com>
2024-09-14 22:29:17 +0000

    Completed part 1 of lab2

---
+++ packages/learning_ros/src/turtle_square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist
+
+rospy.init_node('turtle_square')
+
+pub = rospy.Publisher('/turtlesim/turtle1/cmd_vel', Twist, queue_size=10)
+
+rate = rospy.Rate(1)
+
+def move_square():
+    twist = Twist()
+
+    # Loop to move turtle in square
+    for _ in range(4):
+        # Move forward 2 units
+        twist.linear.x = 2.0
+        twist.angular.z = 0.0
+        pub.publish(twist)
+        rospy.sleep(2)
+
+        # Stop and turn
+        twist.linear.x = 0.0
+        twist.angular.z = 1.571 # approx 90 degrees
+        pub.publish(twist)
+        rospy.sleep(2)
+
+if __name__ == '__main__':
+    try:
+        # slight delay before publishing commands
+        rospy.sleep(1)
+        while not rospy.is_shutdown():
+            move_square()
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
